d549be1c2f4e1fbf674c890ca25ae2dee398367f
esseedev <krzysztoflyda@outlook.com>
2024-07-05 21:51:15 +0200

    fix naming

--- BooksStoreClient.Core/Command/PostBooksCommand.cs
+++ BooksStoreClient.Core/Command/PostBooksCommand.cs
@@ -3,13 +3,14 @@ using Common.Dto;
 
 namespace BooksStoreClient.Core.Command;
 
-public class PostBooksCommand(BooksStoreClient booksStoreClient) : IPostBooksCommand
+public class PostBooksCommandHandler(BooksStoreClient booksStoreClient) : IPostBooksCommand
 {
     private readonly BooksStoreClient _booksStoreClient = 
         booksStoreClient ?? throw new ArgumentNullException(nameof(booksStoreClient));
 
     public async Task Execute(BooksDto newBooks, CancellationToken cancellationToken)
     {
+        // TODO: Create command with implementation
         await _booksStoreClient.PostBooks(newBooks, cancellationToken).ConfigureAwait(false);
     }
 }
